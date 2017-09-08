0bfe797d665d91a3958a7211d97525cbb1003af0
Mitchell Wriedt <mwriedt@optusnet.com.au>
2017-09-08 13:36:43 +1000

    Implement Send/Respond Message
    Home agent sends a message to an appliance agent and the appliance agent
    can reply

--- src/home_electricity_agents/ApplianceAgent.java
+++ src/home_electricity_agents/ApplianceAgent.java
@@ -1,6 +1,10 @@
 package home_electricity_agents;
 
 import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
+
+import javax.swing.*;
 
 public class ApplianceAgent extends Agent {
 	//How much electricity the appliance consumes
@@ -9,4 +13,32 @@ public class ApplianceAgent extends Agent {
 	private int consumeRange = 0;
 	
 	//TODO: add home ID
+	@Override
+	protected void setup()
+	{
+		addBehaviour(new CyclicBehaviour(this) {
+			@Override
+			public void action() {
+				//Receive the other agents message
+				ACLMessage msg=receive();
+				if (msg != null)
+				{
+					//JOptionPane.showMessageDialog(null,"Message received " + msg.getContent());
+					System.out.println(getLocalName()+ ": Received message " +
+					msg.getContent() + " from " + msg.getSender().getLocalName());
+
+					ACLMessage reply = msg.createReply();
+					reply.setPerformative(ACLMessage.INFORM);
+					reply.setContent("Pong");
+
+					System.out.println(getLocalName() + ": Sending response " +
+					reply.getContent() + " to " + msg.getAllReceiver().next());
+
+					send(reply);
+				}
+				else
+					block();
+			}
+		});
+	}
 }
--- src/home_electricity_agents/HomeAgent.java
+++ src/home_electricity_agents/HomeAgent.java
@@ -1,6 +1,12 @@
 package home_electricity_agents;
 
+import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.OneShotBehaviour;
+import jade.lang.acl.ACLMessage;
+
+import java.util.Iterator;
 
 public class HomeAgent extends Agent {
 	//Knows how much electricity it has
@@ -17,4 +23,29 @@ public class HomeAgent extends Agent {
 	
 	private int acceptableSellMax = 0;
 	private int acceptableSellMin = 0;
+	
+	@Override
+	protected void setup()
+	{
+		addBehaviour(new CyclicBehaviour()
+		{
+			@Override
+			public void action()
+			{
+				//Send a message to the other agent
+
+			}
+		});
+
+		ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+		msg.setContent("Send");
+		msg.addReceiver(new AID("APP",AID.ISLOCALNAME));
+		Iterator receivers = msg.getAllIntendedReceiver();
+		System.out.println("Sending message");
+		while(receivers.hasNext())
+		{
+			System.out.println("Sending to : " +((AID)receivers.next()).getLocalName());
+		}
+		send(msg);
+	}
 }
